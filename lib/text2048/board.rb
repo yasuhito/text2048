2caa1ff1827a388382275dcd3eac0af7c09ce635
Yasuhito Takamiya <yasuhito@gmail.com>
2014-05-13 14:20:02 +0900

    Add YARD comments.

--- lib/text2048/board.rb
+++ lib/text2048/board.rb
@@ -50,26 +50,30 @@ module Text2048
 
     # @!group Tiles
 
+    # @return [Array<Tile>] the list of tiles
     def tiles
       @all_tiles.select { |_key, each| each.to_i > 0 }
     end
 
+    # @return [Array] the list of +[row, col]+ of the merged tiles
     def merged_tiles
       find_tiles :merged
     end
 
+    # @return [Array] the list of +[row, col]+ of the newly generated tiles
     def generated_tiles
       find_tiles :generated
     end
 
-    def [](coord)
-      @all_tiles[coord]
-    end
-
+    # Need to generate a new tile?
+    # @param other [Board] the previous {Board} object
+    # @return [Boolean] generate a new tile?
     def generate?(other)
       to_a != other.to_a
     end
 
+    # Generates a new tile
+    # @return [Board] a new board
     def generate
       tiles = @all_tiles.dup
       tiles[sample_zero_tile] = Tile.new(rand < 0.8 ? 2 : 4, :generated)
@@ -92,14 +96,17 @@ module Text2048
 
     # @!group Conversion
 
+    # @return [Array] a 2D array of tiles.
     def to_a
       [0, 1, 2, 3].map { |each| row(each) }
     end
 
+    # @return [Board] a horizontally flipped board
     def flip_horizontal
       new_board(to_a.map(&:reverse), @score)
     end
 
+    # @return [Board] a transposed board
     def transpose
       new_board(to_a.transpose, @score)
     end
