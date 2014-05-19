0fbb5db0d0ec29fb0e88741beffb99d3f3542a62
Yasuhito Takamiya <yasuhito@gmail.com>
2014-05-19 21:59:41 +0900

    Refactor.

--- lib/text2048/curses_view/lcd.rb
+++ lib/text2048/curses_view/lcd.rb
@@ -6,32 +6,34 @@ module Text2048
     # Renders numbers like LCDs
     class LCD
       BITMAPS = [
-        0b1111111110111,
-        0b1010100100100,
-        0b1111111011101,
-        0b1111111101101,
-        0b1011110101110,
-        0b1111111101011,
-        0b1111111111011,
-        0b1010110100101,
-        0b1111111111111,
-        0b1111111101111
+        0b111111110101011,
+        0b101010010001000,
+        0b111111100111001,
+        0b111111110011001,
+        0b101111010011010,
+        0b111111110010011,
+        0b111111110110011,
+        0b101011010001001,
+        0b111111110111011,
+        0b111111110011011
       ]
 
       TOP = 0
       TOP_LEFT = 1
-      TOP_RIGHT = 2
-      MIDDLE = 3
-      BOTTOM_LEFT = 4
-      BOTTOM_RIGHT = 5
-      BOTTOM = 6
+      TOP_MIDDLE = 2
+      TOP_RIGHT = 3
+      MIDDLE = 4
+      BOTTOM_LEFT = 5
+      BOTTOM_MIDDLE = 6
+      BOTTOM_RIGHT = 7
+      BOTTOM = 8
 
-      CORNER_TOP_LEFT = 7
-      CORNER_TOP_RIGHT = 8
-      CORNER_MIDDLE_LEFT = 9
-      CORNER_MIDDLE_RIGHT = 10
-      CORNER_BOTTOM_LEFT = 11
-      CORNER_BOTTOM_RIGHT = 12
+      CORNER_TOP_LEFT = 9
+      CORNER_TOP_RIGHT = 10
+      CORNER_MIDDLE_LEFT = 11
+      CORNER_MIDDLE_RIGHT = 12
+      CORNER_BOTTOM_LEFT = 13
+      CORNER_BOTTOM_RIGHT = 14
 
       def initialize(number)
         @number = number
@@ -47,9 +49,9 @@ module Text2048
       def digit(number)
         lines =
           [[CORNER_TOP_LEFT, TOP, CORNER_TOP_RIGHT],
-           [TOP_LEFT, nil, TOP_RIGHT],
+           [TOP_LEFT, TOP_MIDDLE, TOP_RIGHT],
            [CORNER_MIDDLE_LEFT, MIDDLE, CORNER_MIDDLE_RIGHT],
-           [BOTTOM_LEFT, nil, BOTTOM_RIGHT],
+           [BOTTOM_LEFT, BOTTOM_MIDDLE, BOTTOM_RIGHT],
            [CORNER_BOTTOM_LEFT, BOTTOM, CORNER_BOTTOM_RIGHT]]
         lines.map { |each| line(BITMAPS[number.to_i], each) }
       end
@@ -57,13 +59,7 @@ module Text2048
       # @todo This method smells of :reek:UtilityFunction
       # @todo This method smells of :reek:FeatureEnvy
       def line(bitmap, bits)
-        bits.map do |each|
-          if each
-            (bitmap & 1 << each).zero? ? ' ' : '*'
-          else
-            ' '
-          end
-        end.join
+        bits.map { |each| (bitmap & 1 << each).zero? ? ' ' : '*' }.join
       end
     end
   end
